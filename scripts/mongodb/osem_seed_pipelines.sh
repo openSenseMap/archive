b43f0753c7532a0c58fff285e1291f701d4be290
Matthias Pfeil <matthias.pfeil@wwu.de>
2024-07-02 09:36:00 +0200

    export more collections

--- scripts/mongodb/osem_seed_pipelines.sh
+++ scripts/mongodb/osem_seed_pipelines.sh
@@ -5,11 +5,13 @@ DATABASE=OSeM-api
 PASS=${OSEM_dbuserpass:-"admin"}
 
 echo "Going to run aggregation"
-mongo --username $USER --password $PASS --authenticationDatabase OSeM-api < /functions/aggregations.js
+mongosh --username $USER --password $PASS --authenticationDatabase OSeM-api < /functions/aggregations.js
 echo "Aggregation finished"
 
 echo "Going to export boxes and sensors as csv"
-mongoexport --uri="mongodb://localhost:27017" --db=OSeM-api --collection=device --type=csv --fields="id,name,exposure,use_auth,created_at,updated_at,latitude,longitude" --out="/exports/device.csv"
+mongoexport --uri="mongodb://localhost:27017" --db=OSeM-api --collection=user --type=csv --fields="id,name,email,role,email_is_confirmed,created_at,updated_at" --out="/exports/user.csv"
+mongoexport --uri="mongodb://localhost:27017" --db=OSeM-api --collection=password --type=csv --fields="user_id,hash" --out="/exports/password.csv"
+mongoexport --uri="mongodb://localhost:27017" --db=OSeM-api --collection=device --type=csv --fields="id,name,exposure,use_auth,created_at,updated_at,latitude,longitude,user_id" --out="/exports/device.csv"
 mongoexport --uri="mongodb://localhost:27017" --db=OSeM-api --collection=sensor --type=csv --fields="id,title,sensor_type,unit,device_id" --out="/exports/sensor.csv"
 mongoexport --uri="mongodb://localhost:27017" --db=OSeM-api --collection=measurement --type=csv --fields="sensor_id,time,value" --out="/exports/measurement.csv"
 echo "Export finished"
