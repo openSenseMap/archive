cd449e45e13c3e5a3e8745a0a44b28ea172c83d6
Matthias Pfeil <matthias.pfeil@wwu.de>
2024-10-09 19:34:00 +0200

    add location to measurement export

--- scripts/mongodb/osem_seed_pipelines.sh
+++ scripts/mongodb/osem_seed_pipelines.sh
@@ -14,5 +14,5 @@ mongoexport --uri="mongodb://localhost:27017" --db=OSeM-api --collection=passwor
 mongoexport --uri="mongodb://localhost:27017" --db=OSeM-api --collection=profile --type=csv --fields="id,username,user_id" --out="/exports/profile.csv"
 mongoexport --uri="mongodb://localhost:27017" --db=OSeM-api --collection=device --type=csv --fields="id,name,exposure,use_auth,created_at,updated_at,latitude,longitude,user_id" --out="/exports/device.csv"
 mongoexport --uri="mongodb://localhost:27017" --db=OSeM-api --collection=sensor --type=csv --fields="id,title,sensor_type,unit,device_id" --out="/exports/sensor.csv"
-mongoexport --uri="mongodb://localhost:27017" --db=OSeM-api --collection=measurement --type=csv --fields="sensor_id,time,value" --out="/exports/measurement.csv"
+mongoexport --uri="mongodb://localhost:27017" --db=OSeM-api --collection=measurement --type=csv --fields="sensor_id,time,value,latitude,longitude" --out="/exports/measurement.csv"
 echo "Export finished"
