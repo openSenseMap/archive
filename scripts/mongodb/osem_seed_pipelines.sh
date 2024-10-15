1d706220dc08e71439d8056d2383e3f4d7102626
Matthias Pfeil <matthias.pfeil@wwu.de>
2024-10-15 15:15:58 +0200

    export long and lat

--- scripts/mongodb/osem_seed_pipelines.sh
+++ scripts/mongodb/osem_seed_pipelines.sh
@@ -15,4 +15,5 @@ mongoexport --uri="mongodb://localhost:27017" --db=OSeM-api --collection=profile
 mongoexport --uri="mongodb://localhost:27017" --db=OSeM-api --collection=device --type=csv --fields="id,name,exposure,use_auth,created_at,updated_at,latitude,longitude,user_id" --out="/exports/device.csv"
 mongoexport --uri="mongodb://localhost:27017" --db=OSeM-api --collection=sensor --type=csv --fields="id,title,sensor_type,unit,device_id" --out="/exports/sensor.csv"
 mongoexport --uri="mongodb://localhost:27017" --db=OSeM-api --collection=measurement --type=csv --fields="sensor_id,time,value,latitude,longitude" --out="/exports/measurement.csv"
+mongoexport --uri="mongodb://localhost:27017" --db=OSeM-api --collection=location --type=csv --fields="deviceId,timestamp,longitude,latitude" --out="/exports/location.csv"
 echo "Export finished"
